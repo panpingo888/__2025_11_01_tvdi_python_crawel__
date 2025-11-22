520cb1e7c20e20478a95310b909cae595d11e716
panpingo <panpingo888@gmail.com>
2025-11-22 15:09:17 +0800

    mod

--- lesson4/lesson4_3.py
+++ lesson4/lesson4_3.py
@@ -2,15 +2,20 @@ from playwright.sync_api import sync_playwright
 import os
 from time import sleep
 
-    def main():
-        with sync_playwright() as p:
-            browser = p.chromium.lunch(headless=False,slow_mo=500)
-            page = browser.new_page()
-            current_dir = os.path.dirname(os.path.abspath(__file__))
-            html_file = os.path.join(current_dir,"form_demo.html")
-            page.goto(f"file://{html_file}")
-            sleep(3)
-            browser.close()
+def main():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False,slow_mo=500)
+        page = browser.new_page()
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        html_file = os.path.join(current_dir,"form_demo.html")
+        page.goto(f"file://{html_file}")
+        page.fill("input#name","張三")
+        page.fill("input#email","zhang@example.com")
+        page.select_option("select#country","Taiwan")
+        page.check("input#subscribe")
+        page.click
+        sleep(3)
+        browser.close()
 
-    if __name__ =="_main":
-    
+if __name__ =="__main__":
+    main()
